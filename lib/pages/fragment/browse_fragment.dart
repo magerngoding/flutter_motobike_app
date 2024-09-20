e1365ea60ab47a66eedf2454a647ad20e7370c23
Hafizh <Hafizh@gmail.com>
2024-09-20 08:02:36 +0700

    Membuat browse header, browser categories

--- lib/pages/fragment/browse_fragment.dart
+++ lib/pages/fragment/browse_fragment.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BrowseFragment extends StatelessWidget {
   const BrowseFragment({super.key});
@@ -8,12 +9,110 @@ class BrowseFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "Browse",
-        style: TextStyle(
-          fontSize: 14.0,
-          fontWeight: FontWeight.bold,
+      child: ListView(
+        padding: const EdgeInsets.all(0),
+        children: [
+          Gap(30 + MediaQuery.of(context).padding.top),
+          buildHeader(),
+          Gap(30),
+          buildCategories(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildCategories() {
+    final categories = [
+      ['City', 'assets/ic_city.png'],
+      ['Downhill', 'assets/ic_downhill.png'],
+      ['Beach', 'assets/ic_beach.png'],
+    ];
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Text(
+            "Categories",
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Color(0XFF070623),
+            ),
+          ),
         ),
+        Gap(10),
+        SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: Row(
+              children: categories.map((e) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 14),
+                  padding: const EdgeInsets.fromLTRB(16, 14, 30, 14),
+                  height: 52,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        e[1],
+                        width: 24.0,
+                        height: 24.0,
+                      ),
+                      Gap(10),
+                      Text(
+                        e[0],
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0XFF070623),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildHeader() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            "assets/logo_text.png",
+            height: 38,
+            fit: BoxFit.fitHeight,
+          ),
+          Container(
+            width: 46,
+            height: 46,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+            alignment: Alignment.center,
+            child: Image.asset(
+              "assets/ic_notification.png",
+              width: 24.0,
+              height: 24.0,
+            ),
+          ),
+        ],
       ),
     );
   }
