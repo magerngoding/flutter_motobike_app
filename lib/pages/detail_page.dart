d9b0841e4d4d10d86fe2b787918903b48406843c
Hafizh <Hafizh@gmail.com>
2024-09-28 07:46:42 +0700

    Fix bug color, membuat detail send message

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -2,9 +2,12 @@
 import 'package:d_session/d_session.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_motobike_app/common/info.dart';
 import 'package:flutter_motobike_app/controller/detail_controller.dart';
 import 'package:flutter_motobike_app/models/account.dart';
 import 'package:flutter_motobike_app/models/bike.dart';
+import 'package:flutter_motobike_app/models/chat.dart';
+import 'package:flutter_motobike_app/sources/chat_source.dart';
 import 'package:flutter_motobike_app/widgets/button_primary.dart';
 import 'package:flutter_motobike_app/widgets/failed_ui.dart';
 import 'package:gap/gap.dart';
@@ -48,84 +51,148 @@ class _DetailPageState extends State<DetailPage> {
           Gap(20 + MediaQuery.of(context).padding.top),
           buildHeader(),
           Gap(30),
-          Obx(
-            () {
-              String status = detailController.status;
-              if (status == '') return SizedBox();
-              if (status == 'loading') {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              if (status != 'success') {
-                return Padding(
-                  padding: EdgeInsets.all(24.0),
-                  child: FailedUI(message: status),
-                );
-              }
-              Bike bike = detailController.bike;
+          Obx(() {
+            String status = detailController.status;
+            if (status == '') return SizedBox();
+            if (status == 'loading') {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (status != 'success') {
               return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gap(30),
-                    Center(
-                      child: Text(
-                        bike.name,
-                        style: TextStyle(
-                          fontSize: 32.0,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0XFF07023),
-                        ),
-                      ),
-                    ),
-                    buildStats(bike),
-                    Gap(30),
-                    Stack(
-                      alignment: Alignment
-                          .bottomCenter, // biar lingkaran nya ke bawah motor
-                      children: [
-                        Image.asset(
-                          "assets/ellipse.png",
-                          fit: BoxFit.fitWidth,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 12),
-                          child: ExtendedImage.network(
-                            bike.image,
-                            height: 250,
-                            fit: BoxFit.fitHeight,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Gap(30),
-                    Text(
-                      'About',
+                padding: EdgeInsets.all(24.0),
+                child: FailedUI(message: status),
+              );
+            }
+            Bike bike = detailController.bike;
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      bike.name,
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0XFF07023),
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0XFF070623),
                       ),
                     ),
-                    Gap(10),
-                    Text(
-                      bike.about,
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0XFF07023),
+                  ),
+                  const Gap(10),
+                  buildStats(bike),
+                  Gap(30),
+                  Stack(
+                    alignment: Alignment
+                        .bottomCenter, // biar lingkaran nya ke bawah motor
+                    children: [
+                      Image.asset(
+                        "assets/ellipse.png",
+                        fit: BoxFit.fitWidth,
                       ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 12),
+                        child: ExtendedImage.network(
+                          bike.image,
+                          height: 250,
+                          fit: BoxFit.fitHeight,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Gap(30),
+                  Text(
+                    'About',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0XFF070623),
                     ),
-                    Gap(40),
-                    buildPrice(bike),
-                  ],
-                ),
+                  ),
+                  Gap(10),
+                  Text(
+                    bike.about,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0XFF070623),
+                    ),
+                  ),
+                  Gap(40),
+                  buildPrice(bike),
+                  Gap(16),
+                  buildSendMessage(bike),
+                  Gap(30),
+                ],
+              ),
+            );
+          }),
+        ],
+      ),
+    );
+  }
+
+  Widget buildSendMessage(Bike bike) {
+    return Material(
+      borderRadius: BorderRadius.circular(50),
+      color: const Color(0xffFFFFFF),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(50),
+        onTap: () {
+          String uid = account.uid;
+          Chat chat = Chat(
+            roomId: uid,
+            message: 'Ready?',
+            receiverId: 'cs',
+            senderId: uid,
+            bikeDetail: {
+              'image': bike.image,
+              'name': bike.name,
+              'category': bike.category,
+              'id': bike.id,
+            },
+          );
+
+          Info.netral('Loading...');
+          ChatSource.openChatRoom(uid, account.name).then(
+            (value) {
+              ChatSource.send(chat, uid).then(
+                (value) {
+                  Navigator.pushNamed(context, '/chatting', arguments: {
+                    'uid': uid,
+                    'username': account.name,
+                  });
+                },
               );
             },
+          );
+        },
+        child: SizedBox(
+          width: double.infinity,
+          height: 52,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/ic_message.png",
+                width: 24.0,
+                height: 24.0,
+              ),
+              Gap(10),
+              Text(
+                'Send Message',
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff070623),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
@@ -141,7 +208,7 @@ class _DetailPageState extends State<DetailPage> {
         borderRadius: BorderRadius.all(
           Radius.circular(20.0),
         ),
-        color: Color(0XFF07023),
+        color: Color(0XFF070623),
       ),
       child: Row(
         children: [
@@ -203,18 +270,15 @@ class _DetailPageState extends State<DetailPage> {
         if (e.isEmpty) return Gap(20);
         return Row(
           children: [
-            Image.asset(
-              e[0],
-              width: 24.0,
-              height: 24.0,
-            ),
+            Image.asset(e[0], width: 24.0, height: 24.0),
             Gap(4),
             Text(
               e[1],
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 16.0,
                 fontWeight: FontWeight.w600,
-                color: Color(0XFF07023),
+                color: Color(0XFF070623),
               ),
             ),
           ],
